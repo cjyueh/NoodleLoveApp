bb55930c931149c7e603b2e3444e6220f41e2797
cjyueh <cjyueh@gmail.com>
2016-02-23 17:17:22 -0800

    creates sign-in form and session login

--- noodle_love_app/app/controllers/sessions_controller.rb
+++ noodle_love_app/app/controllers/sessions_controller.rb
@@ -1,2 +1,19 @@
 class SessionsController < ApplicationController
+	#provide form to create a new session (sign-in)
+	def new
+		@user = User.new
+		render :new
+	end
+
+	#create new session (sign in user)
+	def create
+		user_params = params.require(:user).permit(:email, :password)
+		@user = User.confirm(user_params)
+		if @user
+			login(@user)
+			redirect_to "/"
+		else
+			redirect_to "/sign-in"
+		end
+	end
 end
--- noodle_love_app/config/routes.rb
+++ noodle_love_app/config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   get "/users/:id", to: "users#show", as: "user"
 
   get "/sign-in", to: "sessions#new"
-  # post "/sessions", to "sessions#create"
+  post "/sessions", to: "sessions#create"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
